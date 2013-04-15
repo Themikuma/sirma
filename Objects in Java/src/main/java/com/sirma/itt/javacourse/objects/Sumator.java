4f022d042ccc19b8d42ece719aa3dbcdebdba0ea
Nikolai Velkov <metodski2@gmail.com>
2013-04-15 19:41:58 +0300

    WIP First assignment from "Objects in Java"

---
+++ Objects in Java/src/main/java/com/sirma/itt/javacourse/objects/Sumator.java
@@ -0,0 +1,11 @@
+package com.sirma.itt.javacourse.objects;
+
+public class Sumator<N> {
+	public int sum(N numOne, N numTwo) {
+		return numOne + numTwo;
+	}
+
+	public static void main(String[] args) {
+		Sumator<>
+	}
+}
